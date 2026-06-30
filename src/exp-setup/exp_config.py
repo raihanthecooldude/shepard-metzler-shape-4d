463db459967049a6da7ba60172fedad90b3d2912
Raihan Gafur <raihanthecooldude@gmail.com>
2026-06-30 23:54:18 +0300

    fix mirror axes

--- src/exp-setup/exp_config.py
+++ src/exp-setup/exp_config.py
@@ -5,7 +5,7 @@ TRIAL_TYPE = "non-jittering-4d"
 TRIAL_NUM = 1
 SAVE_DIR_IMG = f"exp-results/{SUBJECT_NAME}/{EXP_TYPE}/{TRIAL_TYPE}/{TRIAL_NUM}/images"
 SAVE_DIR_CSV = f"exp-results/{SUBJECT_NAME}/{EXP_TYPE}/{TRIAL_TYPE}/{TRIAL_NUM}/result"
-MIRROR_AXES = "z"
+MIRROR_AXES = "x"
 
 # paths to generate shape (randomized) - 4D (uses O/I 4th-dim moves)
 PATHS_RANDOM = [
